f0b40c964978c9aa88ed5ed37447e9c98ea74cc6
Siraj <sirajmeher984@gmail.com>
2026-04-30 00:29:22 +0500

    Initial commit - Expense Tracker project

---
+++ src/test/java/com/example/expensetrackerdashboard/ExpenseTrackerDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expensetrackerdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
