14ccbbd995723d556b032c77ba8687a858eb8922
Siraj <sirajmeher984@gmail.com>
2026-05-03 20:28:41 +0500

    improve the flow of th ui

--- frontend/src/main/java/com/example/frontend/HelloApplication.java
+++ frontend/src/main/java/com/example/frontend/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 650);
-        stage.setTitle("Expense Tracker — Dashboard");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Expense Tracker — Login");
         stage.setScene(scene);
         stage.show();
     }
